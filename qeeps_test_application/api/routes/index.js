7197fb00112a5787d92fb97311dd1d7a912f83a6
slimanePLF <slimaneber2001@gmail.com>
2023-03-30 13:15:06 +0200

    Setting up routing

---
+++ qeeps_test_application/api/routes/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const health = require('./health');
+const asset = require('./asset');
+const agency = require('./agency');
+const user = require('./user');
+// __IMPORT__
+
+const router = express.Router();
+router.use('/ping', health);
+router.use('/assets', asset);
+router.use('/agencies', agency);
+router.use('/users', user);
+router.use('/assets', asset);
+router.use('/users', user);
+// __ROUTE__
+
+module.exports = router;

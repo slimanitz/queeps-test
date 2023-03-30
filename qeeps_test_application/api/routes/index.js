d96d7ab14dbb630efd6753be50b458e1c696f0a8
slimanePLF <slimaneber2001@gmail.com>
2023-03-31 01:33:21 +0200

    Little fixes

--- qeeps_test_application/api/routes/index.js
+++ qeeps_test_application/api/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const health = require('./health');
 const asset = require('./asset');
 const agency = require('./agency');
 const user = require('./user');
 // __IMPORT__
 
 const router = express.Router();
-router.use('/ping', health);
 router.use('/agencies', agency);
 router.use('/assets', asset);
 router.use('/users', user);

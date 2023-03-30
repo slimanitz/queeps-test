dc6126264874c219cc50be1f73a40c6df7364b72
slimanePLF <slimaneber2001@gmail.com>
2023-03-30 15:45:49 +0200

    Quick fixes

--- qeeps_test_application/api/services/asset.js
+++ qeeps_test_application/api/services/asset.js
@@ -9,44 +9,43 @@ const schema = Joi.object({
   buildYear: Joi.number().required(),
   address: Joi.string().required(),
   city: Joi.string().required(),
-  postalCode: Joi.number().required(),
+  postalCode: Joi.string().required(),
   forSell: Joi.boolean().required(),
   price: Joi.number().required(),
-  agency: Joi.string().required(),
-
 });
 
-const create = async (asset) => {
+const create = async (asset, agency) => {
   const { error, value } = schema.validate(asset);
   if (error) throw new APIError('Bad Payload', httpStatus.BAD_REQUEST);
+  value.agency = agency;
   const newAsset = new Asset(value);
   await newAsset.save();
   return newAsset;
 };
 
-const get = async (id) => {
-  const asset = await Asset.findById(id);
+const get = async (id, agency) => {
+  const asset = await Asset.findOne({ _id: id, agency });
   if (!asset) throw new APIError('No asset found', httpStatus.NOT_FOUND);
   return asset;
 };
 
-const getAll = async () => {
-  const assets = await Asset.find();
+const getAll = async (agency) => {
+  const assets = await Asset.find({ agency });
   return assets;
 };
 
-const update = async (id, payload) => {
+const update = async (id, payload, agency) => {
   const { error, value } = schema.validate(payload);
   if (error) throw new APIError('Bad Payload', httpStatus.BAD_REQUEST);
-  const updatedValue = await Asset.findByIdAndUpdate(id, value);
+  const updatedValue = await Asset.findOneAndUpdate({ _id: id, agency }, value);
   if (!updatedValue) throw new APIError('Not Found', httpStatus.NOT_FOUND);
   return updatedValue;
 };
 
-const remove = async (id) => {
-  const asset = await get(id);
+const remove = async (id, agency) => {
+  const asset = await get(id, agency);
   if (!asset) throw new APIError('No such asset', httpStatus.NOT_FOUND);
-  await Asset.findByIdAndDelete(id);
+  await Asset.findOneAndDelete({ _id: id, agency });
 };
 
 module.exports.assetService = {
